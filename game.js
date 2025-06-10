64ed6c87f00bcc7fe74b6bc93a610c8d9948e309
박상흠 (Sangheum Park) <Sangheum.park.129@shiseido.co.kr>
2025-06-10 10:32:38 +0900

    d

--- game.js
+++ game.js
@@ -25,27 +25,15 @@ function create() {
     // 바닥 생성
     this.matter.add.rectangle(400, 590, 800, 20, { isStatic: true });
 
-    // 화면 상단 중앙에 대포 생성
-    const cannon = this.matter.add.rectangle(400, 50, 80, 40, {
-        isStatic: true
+    // 블록 생성 (정사각형)
+    block = this.matter.add.rectangle(200, 100, 60, 60, {
+        restitution: 0.4,
+        friction: 0.8
     });
 
-    // 마우스 클릭 시 대포에서 클릭 방향으로 블록을 발사
-    this.input.on('pointerdown', pointer => {
-        const block = this.matter.add.rectangle(cannon.position.x, cannon.position.y, 60, 60, {
-            restitution: 0.4,
-            friction: 0.8
-        });
-
-        const direction = new Phaser.Math.Vector2(
-            pointer.x - cannon.position.x,
-            pointer.y - cannon.position.y
-        ).normalize();
-
-        this.matter.body.setVelocity(block, {
-            x: direction.x * 15,
-            y: direction.y * 15
-        });
+    // 마우스 클릭 시 블록을 오른쪽으로 발사
+    this.input.on('pointerdown', () => {
+        this.matter.body.setVelocity(block, { x: 8, y: -10 });
     });
 }
 
