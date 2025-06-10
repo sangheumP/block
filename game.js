151f007c37cf08feb35c825d8ae04cd5a22e10ca
sangheumP <sangheum.park@outlook.com>
2025-06-10 10:37:52 +0900

    대포 추가 및 블록 발사 구현

--- game.js
+++ game.js
@@ -1,23 +1,5 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#ffffff',
-    physics: {
-        default: 'matter',
-        matter: {
-            gravity: { y: 1 },
-            debug: true
-        }
-    },
-    scene: {
-        preload,
-        create,
-        update
-    }
-};
-
-const game = new Phaser.Game(config);
+let block;
+let cannon;
 
 function preload() {}
 
@@ -25,15 +7,19 @@ function create() {
     // 바닥 생성
     this.matter.add.rectangle(400, 590, 800, 20, { isStatic: true });
 
+    // 대포를 화면 위 중앙에 추가 (시각적 도형)
+    cannon = this.add.rectangle(400, 50, 80, 40, 0x000000); // 색상은 검정
+    this.matter.add.gameObject(cannon, { isStatic: true }); // Matter 물리 적용 (optional)
+
     // 블록 생성 (정사각형)
-    block = this.matter.add.rectangle(200, 100, 60, 60, {
+    block = this.matter.add.rectangle(400, 100, 60, 60, {
         restitution: 0.4,
         friction: 0.8
     });
 
-    // 마우스 클릭 시 블록을 오른쪽으로 발사
+    // 마우스 클릭 시 블록 발사
     this.input.on('pointerdown', () => {
-        this.matter.body.setVelocity(block, { x: 8, y: -10 });
+        this.matter.body.setVelocity(block, { x: 0, y: -15 }); // 위로 쏘는 느낌
     });
 }
 
